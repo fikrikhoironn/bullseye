f3a068b0dadd4098e25841c3994516c23ca9dcc1
fikrikhoironn <fikrikf470@gmail.com>
2021-12-19 04:53:04 +0700

    Initial Commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Fikri Khoiron Fadhila on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
